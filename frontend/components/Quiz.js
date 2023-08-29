58d575895a32909399730903762f493527986a93
Austin Amaral <austin.amaral3@gmail.com>
2023-08-29 14:35:53 -0700

    wip

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import { connect, useSelector } from 'react-redux';
+import { fetchQuiz, postAnswer } from '../state/action-creators';
 
-export default function Quiz(props) {
+export function Quiz(props) {
+  const { quiz, selectedAnswer, infoMessage } = props;
   return (
     <div id="wrapper">
       {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
         true ? (
           <>
-            <h2>What is a closure?</h2>
+            <h2></h2>
 
             <div id="quizAnswers">
               <div className="answer selected">
-                A function
+                
                 <button>
                   SELECTED
                 </button>
               </div>
 
               <div className="answer">
-                An elephant
+                
                 <button>
                   Select
                 </button>
@@ -32,3 +35,13 @@ export default function Quiz(props) {
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    quiz: state.quiz,
+    selectedAnswer: state.selectedAnswer,
+    infoMessage: state.infoMessage
+  }
+}
+
+export default connect(mapStateToProps)(Quiz)
--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import * as actionCreators from '../state/action-creators';
+import { useSelector } from 'react-redux';
 
-export default function Wheel(props) {
+export function Wheel(props) {
+  const dispatch = useDispatch()
+  const handleUpClick = () => {
+    dispatch({ type: 'MOVE_CLOCKWISE' })
+  }
+  const handleDownClick = () => {
+    dispatch({ type: 'MOVE_COUNTERCLOCKWISE' })
+  }
+
   return (
     <div id="wrapper">
       <div id="wheel">
@@ -12,9 +23,11 @@ export default function Wheel(props) {
         <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleDownClick}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={handleUpClick}>Clockwise</button>
       </div>
     </div>
   )
 }
+
+export default connect(st => st, actionCreators)(Wheel);
