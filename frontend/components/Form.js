24e267a9556d7290aa03f5b099e229fa88d3fc54
Austin <austin.amaral3@gmail.com>
2023-09-06 08:24:21 -0700

    Form completed

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,24 +1,72 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { postQuiz } from '../state/action-creators';
 
 export function Form(props) {
+  const [formData, setFormData] = useState({
+    newQuestion: '',
+    newTrueAnswer: '',
+    newFalseAnswer: '',
+  });
 
-  const onChange = evt => {
-  }
+  const { newQuestion, newTrueAnswer, newFalseAnswer } = formData;
 
-  const onSubmit = evt => {
+  const onChange = (evt) => {
+    // Update the form data when input values change
+    setFormData({
+      ...formData,
+      [evt.target.id]: evt.target.value,
+    });
+  };
 
-  }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+
+    // Dispatch an action to post the new quiz data to the server
+    props.postQuiz(formData);
+
+    // Clear the form fields after submission
+    setFormData({
+      newQuestion: '',
+      newTrueAnswer: '',
+      newFalseAnswer: '',
+    });
+  };
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn">Submit new quiz</button>
+      <input
+        maxLength={50}
+        onChange={onChange}
+        id="newQuestion"
+        placeholder="Enter question"
+        value={newQuestion}
+      />
+      <input
+        maxLength={50}
+        onChange={onChange}
+        id="newTrueAnswer"
+        placeholder="Enter true answer"
+        value={newTrueAnswer}
+      />
+      <input
+        maxLength={50}
+        onChange={onChange}
+        id="newFalseAnswer"
+        placeholder="Enter false answer"
+        value={newFalseAnswer}
+      />
+      <button id="submitNewQuizBtn" type="submit">
+        Submit new quiz
+      </button>
     </form>
-  )
+  );
 }
 
-export default connect(st => st)(Form)
+const mapDispatchToProps = {
+  postQuiz,
+};
+
+export default connect(null, mapDispatchToProps)(Form);
+
